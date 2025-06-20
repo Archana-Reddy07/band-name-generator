e580a85e95e3685c2127d8117b961ea02fa9657e
Archana Reddy <archanareddychinnapatlola@gmail.com>
2025-06-20 21:47:55 +0530

    Add band name generator project

---
+++ Day1.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator.")
+city=input("Which city do you grow up in?\n")
+pet=input("what is your pet name?\n")
+print("Your band name could be:" + city + " " + pet)
